0f361fcab77e0bdd1ce2cd3d80d6c67fc1c7c997
paras11917 <paras11917@gmail.com>
2023-12-20 18:08:22 +0530

    first commit

--- models/base_request_model.py
+++ models/base_request_model.py
@@ -18,7 +18,7 @@ class BaseSDRequestRoop(BaseModel):
     height: int = 512
     width: int=512
     negative_prompt: str = "deformed, blurr"
-    base_model:str = "SG161222/RealVisXL_V2.0"
+    base_model:str = "SG161222/Realistic_Vision_V1.4"
     num_inference_steps: int = 30
     guidance_scale:float = 7.5
     num_images_per_prompt:int = 1
