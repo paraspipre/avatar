a1bf6fbe24b5de0e701809f4c37f5b8ce445e1b2
paras11917 <paras11917@gmail.com>
2024-01-17 05:10:21 +0530

    commit

--- pipeline/generateImage.py
+++ pipeline/generateImage.py
@@ -187,7 +187,7 @@ def get_roop_enhanced_image(user_image_path, generated_image_path,req_id):
 
     try:
         subprocess.run("pwd", shell=True, check=True)
-        command = "cd {} && python run.py -s ../{} -t ../{} -o ../{}".format("./facefusion",user_image_path, generated_image_path, roop_image_path)
+        command = "cd . {} && python run.py -s ../{} -t ../{} -o ../{}".format("./facefusion",user_image_path, generated_image_path, roop_image_path)
         subprocess.run(command, shell=True, check=True)
     except subprocess.CalledProcessError as e:
         # Reset the working directory to the original directory
