bb407cfc57162d1cd2652ae2cf15ef90b7ea6d0a
paraspipre <70134657+paraspipre@users.noreply.github.com>
2024-03-24 19:11:39 +0530

    Update base_request_model.py

--- models/base_request_model.py
+++ models/base_request_model.py
@@ -18,7 +18,7 @@ class BaseSDRequestRoop(BaseModel):
     height: int = 512
     width: int=512
     negative_prompt: str = "deformed, blurr"
-    base_model:str = "JoPmt/Txt2Img_Jggrnt_XL_V7_Pipe"
+    base_model:str = "lykon/dreamshaper-7"
     num_inference_steps: int = 30
     guidance_scale:float = 7.5
     num_images_per_prompt:int = 1
