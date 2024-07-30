0d192e343284235c7bc619f97a86dc07f7615dd6
paraspipre <paras11917@gmail.com>
2024-07-30 19:52:47 +0530

    commit

--- models/base_request_model.py
+++ models/base_request_model.py
@@ -38,11 +38,11 @@ class BaseSDRequestRoop(BaseModel):
 
 class BaseSDRequestVideo(BaseModel):
     prompt: str ="superman"
-    encoded_image: str
+    # encoded_image: str
     height: int = 512
     width: int=512
     negative_prompt: str = "deformed, nsfw, blurr"
-    base_model:str = "stabilityai/stable-diffusion-xl-base-1.0"
+    base_model:str = "lykon/dreamshaper-7"
     num_inference_steps: int = 30
     guidance_scale:float = 7.5
     num_images_per_prompt:int = 1
